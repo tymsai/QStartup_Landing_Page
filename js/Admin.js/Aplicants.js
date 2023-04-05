75a43af4159863f0a688176fb9b749dbec5745c0
MohammadNasir520 <nasirahsan520@gmail.com>
2023-04-05 19:50:50 +0600

    table inner html table make

--- js/Admin.js/Aplicants.js
+++ js/Admin.js/Aplicants.js
@@ -5,9 +5,36 @@ const loadAllApplicants = () => {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            displayApplicants(data)
         })
         .catch(error => {
             console.log(error)
         })
 }
 loadAllApplicants()
+
+const displayApplicants = (applicants) => {
+    console.log(applicants)
+
+    const aplicantTbody = document.querySelector('#aplicantTbody')
+
+    const row = document.createElement('tr')
+
+    row.innerHTML = `
+    
+   
+                  <td>Sai</td>
+                  <td>
+                    <i class="fa fa-download" aria-hidden="true"></i>
+                    <i style="margin-left: 1rem;" class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                  <td>Frontend Developer</td>
+                  <td>25/12/1991</td>
+               
+              
+    `
+
+    aplicantTbody.appendChild(row)
+    console.log(aplicantTbody)
+
+}
